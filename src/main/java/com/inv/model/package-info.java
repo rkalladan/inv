a2150502b74856ef3ba75ff5ddc057ca657cbc61
rahulkalladan <rahul.kalladan@timeinc.com>
2015-05-08 20:12:29 +0530

    working copy for creating schema and tables. Errors rectified and comments mentioned in readme.txt.

--- src/main/java/com/inv/model/package-info.java
+++ src/main/java/com/inv/model/package-info.java
@@ -1,5 +1,8 @@
 /**
  * @author kalladanr
- *
+ *create table INV_Role (
+ *role_id bigint not null auto_increment,
+ *rolename varchar(255),  primary key (role_id) ) creates tables in db with autoincr if used @GeneratedValue 
+
  */
 package com.inv.model;
